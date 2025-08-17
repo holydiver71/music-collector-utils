1e5e6c31d021ee5a41f14b4047e8ed8080513a33
holydiver71 <andy.shutt@gmail.com>
2025-08-17 16:53:03 +0100

    Add LabelNumber property to AlbumLoader for enhanced album data parsing

--- Utilities/AlbumLoader.cs
+++ Utilities/AlbumLoader.cs
@@ -230,6 +230,7 @@ namespace Utilities
                 LabelId = labelId,
                 Genres = genreIds.Count > 0 ? genreIds : null,
                 Artists = artistIds.Count > 0 ? artistIds : null,
+                LabelNumber = (string?)music.Element("labelnumber") ?? string.Empty,
                 CoverFront = GetFileNameFromPath((string?)music.Element("coverfront")),
                 DateAdded = ParseDate(music.Element("dateadded")?.Element("date")?.Value),
                 LastModified = ParseDate(music.Element("lastmodified")?.Element("date")?.Value),
